568966a98aa529007339ca1f50f6b0cad21a6748
Kiran Kumar Das <kirankumardas224@gmail.com>
2019-10-11 09:59:54 +0530

    update

---
+++ public/precache-manifest.c144305e3d9207d6321330d4763a5946.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c01d02830ae2f5b439900e267655e4b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae571a2127b595038ba5",
+    "url": "/static/css/main.f64226bc.chunk.css"
+  },
+  {
+    "revision": "d8a15cbb85444b4be043",
+    "url": "/static/js/2.05895c38.chunk.js"
+  },
+  {
+    "revision": "ae571a2127b595038ba5",
+    "url": "/static/js/main.dbe1bc14.chunk.js"
+  },
+  {
+    "revision": "9e8e7b00e600cf1b4caa",
+    "url": "/static/js/runtime-main.de53be39.js"
+  },
+  {
+    "revision": "7ab716e57404b80523562c9f24efba52",
+    "url": "/static/media/i1.7ab716e5.jpg"
+  },
+  {
+    "revision": "fe97b2f02204b98cc2616f2016a967cb",
+    "url": "/static/media/i2.fe97b2f0.jpg"
+  },
+  {
+    "revision": "29212f5ee6f1829c59a8b4735f2e513e",
+    "url": "/static/media/main.29212f5e.jpg"
+  }
+]);
