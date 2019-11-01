d0afd3db6db03e1aed0588e448c1f073aa91e522
Kiran Kumar Das <kirankumardas224@gmail.com>
2019-11-01 13:10:26 +0530

    Clean master

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'; 
-import User from './user';
-import Common from './common';
-
-export default combineReducers({
-    user : User,
-    common : Common  
-});
--- src/theme.js
+++
@@ -1,7 +0,0 @@
-export default theme ={
-    $color1: "#EDF5E1",
-    $color2: "#8EE4AF",
-    $color3: "#5CDB95",
-    $color4: "#379683",
-    $color5: "#05386B"
-}
